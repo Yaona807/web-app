7c756a54ae14da4948dc36bd42fc9651960fc85e
i-708 <72598968+i-708@users.noreply.github.com>
2021-06-29 23:21:51 +0900

    ホーム画面の作成及び画面遷移を可能にした

--- app.py
+++ app.py
@@ -13,14 +13,30 @@ class Result(db.Model):
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('home.html')
 
-@app.route('/',methods=['POST'])
+@app.route('/game')
+def start():
+    return render_template('game.html')
+
+@app.route('/game',methods=['POST'])
 def end():
+    max_save_count = 100
     user_name = request.form.get('name')
     score = request.form.get('end')
 
     new_result = Result(user_name=user_name, score=score)
+
+    results = Result.query.order_by(Result.score.desc()).all()
+
+    # ランキングの最大数まで保存していた場合
+    if len(results) >= max_save_count:
+        # 一番下のランクデータを削除(新しいデータが大きい場合のみ)
+        if results[-1].score < int(new_result.score):
+            db.session.delete(results[-1])
+        else:
+            return redirect('/ranking')
+
 
     db.session.add(new_result)
     db.session.commit()
@@ -29,9 +45,10 @@ def end():
 
 @app.route('/ranking')
 def ranking_print():
-    results = Result.query.all()
+    # scoreを用いて降順ソート
+    results = Result.query.order_by(Result.score.desc()).all()
+    print(results[0].user_name)
     return render_template('ranking.html',results=results)
-
 
 
 if __name__ == '__main__':

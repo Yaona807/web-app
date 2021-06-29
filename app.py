2a6da2e67c8b5c14ce86eead60bbacb160f45065
i-708 <72598968+i-708@users.noreply.github.com>
2021-06-29 22:04:36 +0900

    データベースに結果を格納できるようにした

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ranking.db'
+db = SQLAlchemy(app)
 
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(20),nullable=False)
+    score = db.Column(db.Integer)
 
 @app.route('/')
 def main():
@@ -10,8 +17,21 @@ def main():
 
 @app.route('/',methods=['POST'])
 def end():
-    result_str = ''.join([request.form.get('name'),':',request.form.get('end')])
-    return result_str
+    user_name = request.form.get('name')
+    score = request.form.get('end')
+
+    new_result = Result(user_name=user_name, score=score)
+
+    db.session.add(new_result)
+    db.session.commit()
+
+    return redirect('/ranking')
+
+@app.route('/ranking')
+def ranking_print():
+    results = Result.query.all()
+    return render_template('ranking.html',results=results)
+
 
 
 if __name__ == '__main__':

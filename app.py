ee9b7d01a3ef6f64bfa1fb18fa89552a024b7982
i-708 <72598968+i-708@users.noreply.github.com>
2021-07-01 01:02:20 +0900

    PostgreSQLを使用するために書き換え

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ranking.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql://localhost/ranking'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
